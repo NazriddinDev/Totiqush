7555fd751914f0ace91739e8235e053f90b7ee2c
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-05-19 08:51:49 +0500

    TEACH: Use if statement

--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
-﻿Console.Write("Ismingizni kiriting: ");
+﻿using System;
+
+Console.Write("Ismingizni kiriting: ");
 string ism = Console.ReadLine();
 string salomlashish=$"Salom {ism}";
 
 Console.Write("Yoshingizni kiriting: ");
 int Sizningyosh= int.Parse(Console.ReadLine());
-
 int TotiqushYoshi = 2;
 int yoshlarFarqi = Sizningyosh-TotiqushYoshi;
-Console.WriteLine($"Birinchi raqam ikkinchi raqamdan katta {Sizningyosh > TotiqushYoshi}");
-Console.WriteLine($"Birinchi raqam ikkinchi raqamdan katta yoki teng {Sizningyosh >= TotiqushYoshi}");
-Console.WriteLine($"Birinchi raqam ikkinchi raqamdan kichik {Sizningyosh > TotiqushYoshi}");
-Console.WriteLine($"Birinchi raqam ikkinchi raqamdan kichik yoki teng {Sizningyosh <= TotiqushYoshi}");
-Console.WriteLine($"Birinchi raqam ikkinchi raqam teng {Sizningyosh == TotiqushYoshi}");
-Console.WriteLine($"Birinchi raqam ikkinchi raqam teng emas {Sizningyosh != TotiqushYoshi}");
+Console.WriteLine($"Yoshlar farqi {yoshlarFarqi}");
 
+if(Sizningyosh>TotiqushYoshi)
+{
+    Console.WriteLine("Siz kattasiz...");
+}
+else if(Sizningyosh==TotiqushYoshi)
+{
+    Console.WriteLine("Yoshlaringiz teng...");
+}
+else
+{
+    Console.WriteLine("Siz yoshsiz...");
+}
+
+// Console.WriteLine($"Birinchi raqam ikkinchi raqamdan katta {Sizningyosh > TotiqushYoshi}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqamdan katta yoki teng {Sizningyosh >= TotiqushYoshi}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqamdan kichik {Sizningyosh > TotiqushYoshi}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqamdan kichik yoki teng {Sizningyosh <= TotiqushYoshi}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqam teng {Sizningyosh == TotiqushYoshi}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqam teng emas {Sizningyosh != TotiqushYoshi}");
