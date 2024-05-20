a464393ccb4bb8ccfd750647a83e1a7c02577c2f
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-05-20 05:35:03 +0500

    UNFRA: Use ternary operator

--- Program.cs
+++ Program.cs
@@ -9,19 +9,24 @@ int Sizningyosh= int.Parse(Console.ReadLine());
 int TotiqushYoshi = 2;
 int yoshlarFarqi = Sizningyosh-TotiqushYoshi;
 Console.WriteLine($"Yoshlar farqi {yoshlarFarqi}");
+//ternary operator
+string natija =
+    yoshlarFarqi >= 0
+        ? "Sizning yoshingiz to'tiqushning yoshidan katta yok teng"
+        : "Sizning yoshingiz to'tiqushning yoshidan kichik";
 
-if(Sizningyosh>TotiqushYoshi)
-{
-    Console.WriteLine("Siz kattasiz...");
-}
-else if(Sizningyosh==TotiqushYoshi)
-{
-    Console.WriteLine("Yoshlaringiz teng...");
-}
-else
-{
-    Console.WriteLine("Siz yoshsiz...");
-}
+// if(Sizningyosh>TotiqushYoshi)
+// {
+//     Console.WriteLine("Siz kattasiz...");
+// }
+// else if(Sizningyosh==TotiqushYoshi)
+// {
+//     Console.WriteLine("Yoshlaringiz teng...");
+// }
+// else
+// {
+//     Console.WriteLine("Siz yoshsiz...");
+// }
 
 // Console.WriteLine($"Birinchi raqam ikkinchi raqamdan katta {Sizningyosh > TotiqushYoshi}");
 // Console.WriteLine($"Birinchi raqam ikkinchi raqamdan katta yoki teng {Sizningyosh >= TotiqushYoshi}");
