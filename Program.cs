ee5e022bacbcf83e47c37ea46f92a235733a4360
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-05-21 15:09:15 +0500

    INFRA: use logical operator

--- Program.cs
+++ Program.cs
@@ -9,11 +9,31 @@ int Sizningyosh= int.Parse(Console.ReadLine());
 int TotiqushYoshi = 2;
 int yoshlarFarqi = Sizningyosh-TotiqushYoshi;
 Console.WriteLine($"Yoshlar farqi {yoshlarFarqi}");
-//ternary operator
-string natija =
-    yoshlarFarqi >= 0
-        ? "Sizning yoshingiz to'tiqushning yoshidan katta yok teng"
-        : "Sizning yoshingiz to'tiqushning yoshidan kichik";
+// //ternary operator
+// string natija =
+//     yoshlarFarqi >= 0
+//         ? "Sizning yoshingiz to'tiqushning yoshidan katta yok teng"
+//         : "Sizning yoshingiz to'tiqushning yoshidan kichik";
+//         Console.WriteLine(natija);
+
+// Logical operator &&, ||, !
+string message =
+    (Sizningyosh > 0 && TotiqushYoshi > 0)
+        ? "Sizning va to'tiqushning yoshi noldan katta..."
+        : "Sizning yoki to'tiqushning yoki ikkalangizningham yoshlaringiz noldan kichik...";
+        Console.WriteLine(message);
+
+string result =
+    (Sizningyosh > 0 || TotiqushYoshi > 0)
+        ? "Sizning yoki to'tiqushning yoshi noldan katta..."
+        : "Sizning va to'tiqushning yoshi noldan kichik...";
+        Console.WriteLine(result);
+
+string show =
+    !(Sizningyosh > 0 || TotiqushYoshi > 0)
+        ? "Sizning yoki to'tiqushning yoshi noldan katta..."
+        : "Sizning yoki to'tiqushning yoshi noldan kichik...";
+        Console.WriteLine(show);
 
 // if(Sizningyosh>TotiqushYoshi)
 // {
